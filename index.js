274ee485a6f3e4d1c1a1252381d525d7c80eab95
EE Dev 3 <lucijarocks@gmail.com>
2018-08-01 15:21:41 +0200

    Relation_fix_between_User_Quizzes

--- index.js
+++ index.js
@@ -23,23 +23,66 @@ sequelize
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
-const User = sequelize.define('user', {
-  email: Sequelize.STRING,
-  password: Sequelize.STRING
-  // quizId: Sequelize.ARRAY(Sequelize.ENUM)
-});
+
 const Question = sequelize.define('question', {
   topic: Sequelize.TEXT, // .STRING je premal za sve podatke
   course: Sequelize.TEXT,
   fullQuestion: Sequelize.TEXT,
   answers: Sequelize.JSON
 });
+const User = sequelize.define('user', {
+  userId: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
+  email: {
+    type: Sequelize.STRING
+    // validate: {
+    //   isEmail: true
+    // }
+  },
+  password: {
+    type: Sequelize.STRING
+  }
+});
 const Quiz = sequelize.define('quiz', {
-  pickedQuestions: Sequelize.ARRAY(Sequelize.INTEGER),
-  wronglyAnswered: Sequelize.ARRAY(Sequelize.INTEGER),
-  userId: Sequelize.INTEGER
+  pickedQuestions: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    allowNull: true,
+    defaultValue: null
+  },
+  wronglyAnswered: {
+    type: Sequelize.ARRAY(Sequelize.INTEGER),
+    allowNull: true,
+    defaultValue: null
+  }
+});
+User.hasMany(Quiz);
+
+User.sync({ force: true }).then(() => {
+  User.create({
+    email: 'mojemail@gmail.com',
+    password: 'password1234'
+  });
+  User.create({
+    email: 'novi@gmail.com',
+    password: 'password4567'
+  });
 });
 
+Quiz.sync({ force: true }).then(() => {
+  Quiz.create({
+    userUserId: 1,
+    pickedQuestions: [1, 2, 3, 6, 8, 10, 12, 18, 300, 459],
+    wronglyAnswered: [2, 3]
+  });
+  Quiz.create({
+    userUserId: 1,
+    pickedQuestions: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    wronglyAnswered: [2, 3]
+  });
+});
 Question.sync({ force: true }).then(() => {
   const questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
   questions.forEach(question => {
@@ -51,31 +94,3 @@ Question.sync({ force: true }).then(() => {
     });
   });
 });
-
-User.sync({ force: true }).then(() => {
-  User.create({
-    userId: 1,
-    email: 'mojemail@email.com',
-    password: 'password1234'
-  });
-  User.create({
-    userId: 2,
-    email: 'novi@email.com',
-    password: 'password4567'
-  });
-});
-
-Quiz.sync({ force: true }).then(() => {
-  Quiz.create({
-    userId: 1,
-    pickedQuestions: [1, 2, 3, 6, 8, 10, 12, 18, 300, 459],
-    wronglyAnswered: [2, 3]
-  });
-});
-
-// async function Relationship() {
-//   await User;
-//   await Quiz;
-//   User.hasMany(Quiz, {foreignKey: 'userId', sourceKey: 'isoCode'});
-//   Quiz.belongsTo(User, {foreignKey: 'userId', sourceKey: 'isoCode'});
-// }

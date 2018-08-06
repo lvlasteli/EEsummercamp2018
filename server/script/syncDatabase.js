d6db5b0c61806002f6b0f6f00fe355bf039e2fd5
jlasic <lasic.josip96@gmail.com>
2018-08-06 23:27:30 +0200

    Set relations and create database sync script

---
+++ server/script/syncDatabase.js
@@ -0,0 +1,14 @@
+const database = require('../database');
+
+// create models
+require('../question/question.model');
+const User = require('../user/user.model');
+const Quiz = require('../quiz/quiz.model');
+
+// set relations
+Quiz.belongsTo(User, {foreignKey: 'userId'});
+
+// sync database
+database.sync({force: true})
+  .then(() => process.exit(0))
+  .catch(() => process.exit(1));

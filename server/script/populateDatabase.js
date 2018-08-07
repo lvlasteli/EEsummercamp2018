c00853277e568779710d66f70046c14d2fb2e209
EE Dev 3 <lucijarocks@gmail.com>
2018-08-07 20:22:27 +0200

    Added script to create and than populate db

--- server/script/populateDatabase.js
+++ server/script/populateDatabase.js
@@ -1,6 +1,6 @@
-// const syncDB = require('./syncDatabase');
 const Question = require('../question/question.model');
+const database = require('../database');
 const fs = require('fs');
 
 const questions = JSON.parse(fs.readFileSync('questions.json', 'utf8'));
-Question.bulkCreate(questions);
+Question.bulkCreate(questions).then(() => database.close());

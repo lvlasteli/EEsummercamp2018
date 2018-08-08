f11fd0f0c7a85cbe03c46ad9a09d9773494adf48
jlasic <lasic.josip96@gmail.com>
2018-08-08 21:01:29 +0200

    Update client eslint config

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,17 +1,58 @@
-module.exports = {
+module.exports = module.exports = {
   root: true,
-  env: {
-    node: true
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
+  extends: [
+    'plugin:vue/recommended'
+  ],
+
+  plugins: [
+    'vue'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
+    'vue/html-closing-bracket-newline': ['error', {
+        singleline: 'never',
+        multiline: 'never'
+    }],
+    'vue/html-self-closing': 0,
+    'vue/attribute-hyphenation': 0,
+    'vue/max-attributes-per-line': [2, { singleline: 5 }],
+    'vue/name-property-casing': [2, 'kebab-case'],
+    'vue/attributes-order': [2, {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'UNIQUE',
+        'BINDING',
+        'EVENTS',
+        'CONTENT',
+        'GLOBAL',
+        'OTHER_ATTR'
+      ]
+    }],
+    'vue/order-in-components': [2, {
+      order: [
+        'el',
+        'name',
+        ['template', 'render', 'renderError'],
+        ['parent','functional', 'delimiters', 'comments'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'methods',
+        'LIFECYCLE_HOOKS',
+        'watch',
+        ['directives', 'filters'],
+        'components'
+      ]
+    }]
   }
 }

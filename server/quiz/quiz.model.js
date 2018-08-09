92085a9345ebd46f8173b533b7913998d50c26f8
jlasic <lasic.josip96@gmail.com>
2018-08-10 00:42:32 +0200

    Add N to M relation QuizQuestions

--- server/quiz/quiz.model.js
+++ server/quiz/quiz.model.js
@@ -2,7 +2,6 @@ const Sequelize = require('sequelize');
 const database = require('../database');
 
 const Quiz = database.define('quiz', {
-  questions: Sequelize.ARRAY(Sequelize.JSONB),
   percentage: Sequelize.DOUBLE,
   timestamp: Sequelize.DATE,
   elapsedTime: Sequelize.INTEGER
@@ -10,6 +9,19 @@ const Quiz = database.define('quiz', {
 
 // set relations
 const User = require('../user/user.model');
+const Question = require('../question/question.model');
+
+// 1 to N
 Quiz.belongsTo(User, {foreignKey: 'userId'});
+User.hasMany(Quiz, {foreignKey: 'userId'});
+
+// N to M
+// creates new table quizQuestions with these additional columns
+const QuizQuestions = database.define('quizQuestions', {
+  correct: Sequelize.BOOLEAN,
+  answers: Sequelize.ARRAY(Sequelize.INTEGER)
+});
+Question.belongsToMany(Quiz, {through: QuizQuestions});
+Quiz.belongsToMany(Question, {through: QuizQuestions});
 
 module.exports = Quiz;

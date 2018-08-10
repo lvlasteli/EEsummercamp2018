b59a3f04732b9d19b9cb02bb4d5854df44ed9ef0
EE Dev 3 <lucijarocks@gmail.com>
2018-08-10 14:45:01 +0200

    Script that takes user id and makes him a quiz

--- server/quiz/quiz.controller.js
+++ server/quiz/quiz.controller.js
@@ -1,22 +1,2 @@
-const getRandomQ = require('./get10RandomQuestions');
-const Quiz = require('./quiz.model');
-const database = require('../database');
 
-async function saveQuestionsForQuiz(userID) {
-  let pickedQuestions = [];
-  pickedQuestions = await getRandomQ;
-  return Quiz.create({
-    questions: pickedQuestions,
-    percentage: null,
-    timestamp: null,
-    elapsedTime: null,
-    userId: userID // error doesnt save
-  }).then(() => {
-    database.close();
-    // future function that'll go from 0-10 trough a list and show question on a view
-  }).catch((e) => {
-    console.log(e);
-    database.close();
-  });
-}
-saveQuestionsForQuiz();
+
---
+++ server/quiz/script.js
@@ -0,0 +1,38 @@
+const Question = require('../question/question.model');
+const Quiz = require ('./quiz.model');
+const randomInt = require('random-int');
+
+function get10RandomIds() {
+  const chosenNumberOfQuestions = 10;
+  return Question.count().then((numberOfAllExistingQuestions) => {
+    const randIDs = [];
+    while (randIDs.length < chosenNumberOfQuestions) {
+      const broj = randomInt(1, numberOfAllExistingQuestions);
+      if (randIDs.indexOf(broj) === -1) {
+        randIDs.push(broj);
+      }
+    }
+    return randIDs;
+  });
+}
+
+async function saveQuiz(userID) {
+  let randomIds = [];
+  randomIds = await get10RandomIds();
+  console.log(randomIds);
+  Quiz.create({
+    percentage: null,
+    timestamp: null,
+    elapsedTime: null,
+    userId: userID
+  }).then((insertedId) => {
+    console.log(insertedId.id);
+    insertQuestions(randomIds,insertedId.id);
+  });
+}
+
+function insertQuestions(arrayIds, id) {
+
+}
+
+saveQuiz(1);

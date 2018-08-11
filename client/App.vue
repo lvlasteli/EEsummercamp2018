237787042c9313e9b999749d1409c127c5f71bc3
moomy94 <smihan00@fesb.hr>
2018-08-11 19:16:17 +0200

    basic UI design in vue

--- client/App.vue
+++ client/App.vue
@@ -1,28 +1,73 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="wrapper">
+
+    <h1 class="title">{{ msg }}</h1>
+    <br> <br>
+    <h2 class="continue">To continue using this app please log in</h2>
+    <br> <br>
+    <div class="button-container">
+      <button>Login<img width="50" height="50" src="./assets/google_plus.png" align="right"> </button>
+
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      msg: 'Test Your Knowledge'
+    }
   }
-};
+}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+.wrapper {
+  padding-top: 200px;
 }
+
+.button-container {
+  margin: 0 auto;
+  width: 240px;
+}
+
+button{
+  background: #F2F2F2;
+  padding: 20px;
+  width: 70%;
+  color: black;
+  font-family: Segoe UI;
+  font-style: Regular;
+  text-align: center;
+  font-size: 25px;
+  border-image-source: none;
+}
+
+.title{
+  color: black;
+  font-family: Segoe UI;
+  font-style: Regular;
+  text-align: center;
+  width: 97%;
+}
+
+.continue{
+  color: black;
+  font-family: Segoe UI;
+  font-style: Regular;
+  text-align: center;
+  width: 97%;
+}
+
+img{
+  display: block;
+  max-width: 35px;
+  max-height: 35px;
+  width: auto;
+  height: auto;
+}
+
 </style>

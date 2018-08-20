034e1b7751a5e92556ceb713adf120aa5746cd7d
EE Dev 3 <lucijarocks@gmail.com>
2018-08-20 23:38:33 +0200

    questions api

--- server/question/index.js
+++ server/question/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const controller = require('./question.controller');
 
+router.get('/questions/topics/:topic', controller.getQuestionsOfSpecificTopic);
+router.get('/questions/topics', controller.getTopics);
 router.get('/questions/:id', controller.getQuestion);
 
 module.exports = router;
--- server/question/question.controller.js
+++ server/question/question.controller.js
@@ -7,7 +7,21 @@ function getQuestion(req, res) {
     .then((question) => res.json(question))
     .catch(() => res.status(404).json({}));
 }
+function getTopics(req, res) {
+  Question.findAll({ attributes: ['topic'], group: ['topic'] })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(404).send(err.message));
+}
+
+function getQuestionsOfSpecificTopic(req, res) {
+  const nameOfTopic = req.params.topic;
+  Question.findAll({where: {topic: nameOfTopic}})
+    .then((question) => res.json(question))
+    .catch((err) => res.status(404).send(err.message));
+}
 
 module.exports = {
-  getQuestion
+  getQuestion,
+  getTopics,
+  getQuestionsOfSpecificTopic
 };

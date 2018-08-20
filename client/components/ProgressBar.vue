bd5eb4788ecb2f5a9c76144cd475dd1fddc2e78b
moomy94 <smihan00@fesb.hr>
2018-08-20 19:13:45 +0200

    added colours

--- client/components/ProgressBar.vue
+++ client/components/ProgressBar.vue
@@ -1,10 +1,10 @@
 <template>
-  <v-btn-toggle v-model="current">
+  <v-btn-toggle v-model="selected">
     <v-btn
       v-for="(qq, index) in quizQuestions"
       :key="qq.questionId"
       @click="chooseQuestion(index)"
-      color="">Question {{ index + 1 }}</v-btn>
+      :color="answered[index] ? 'orange' : ''">Question {{ index + 1 }}</v-btn>
   </v-btn-toggle>
 </template>
 
@@ -13,6 +13,17 @@ export default {
   props: {
     current: { type: Number, required: true },
     quizQuestions: {type: Array, required: true}
+  },
+  computed: {
+    selected: {
+      get: function () { return this.current; },
+      set: function () {}
+    },
+    answered() {
+      return this.quizQuestions.map(qq => {
+        return qq.answers && qq.answers.every(answer => answer !== null);
+      });
+    }
   },
   methods: {
     chooseQuestion(jump) {
